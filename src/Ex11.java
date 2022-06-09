6650c933986b1aa0bae893aaab884f6dac9f6954
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-09 16:34:07 +0200

    Ex11

---
+++ src/Ex11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        var inp=new Scanner(System.in);
+        System.out.println("Factoriel de :");
+        var n=inp.nextShort();
+        int result=1;
+        if(n==0)
+        {
+            System.out.println(result);
+        }
+        else if(n<0)
+        {
+            System.out.println("La valeur doit étre positive");
+        }
+        else{
+            for (short i=1;i<=n;i++)
+            {
+                result = i*result;
+            }
+            System.out.println(result);
+        }
+    }
+}
