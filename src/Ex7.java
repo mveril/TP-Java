75412c82428c39c3395698fd5fa7aba9252e6157
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-09 12:35:04 +0200

    Ex7

--- src/Ex7.java
+++ src/Ex7.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Ex7 {
     public static void main(String[] args) {
-
+        var input = new Scanner(System.in);
+        System.out.print("Entrer un nombre :");
+        var number= input.nextInt();
+        var isPair = (number % 2) == 0;
+        String stringSign;
+        if (number>0){
+            stringSign= "positif";
+        } else if (number<0){
+            stringSign ="négatif";
+        } else {
+            stringSign= "égal a 0";
+        }
+        System.out.printf("Le nombre est %s est il est %s", stringSign, isPair ? "pair" :  "impair" );
     }
 }
