ee7a5616d7b752dd2b1062e9ace368d13107ba1d
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-09 13:45:56 +0200

    Ex8

--- src/Ex8.java
+++ src/Ex8.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Ex8 {
+    public static void main(String[] args) {
+        var input=new Scanner(System.in);
+        System.out.print("a : ");
+        var a=input.nextDouble();
+        System.out.print("b : ");
+        var b =input.nextDouble();
+        System.out.print("c : ");
+        var c=input.nextDouble();
+        var delta=Math.pow(b,2)-4*a*c;
+        if (delta < 0){
+            System.out.println("Il n'y a pas de solution");
+        }
+        else if (delta == 0)
+        {
+            System.out.printf("La solution est %.5f\n", -b/2*a);
+        }
+        else
+        {
+            System.out.printf("Les solutions sont %.5f et %.5f\n",(-b-Math.sqrt(delta))/2*a,(-b+Math.sqrt(delta))/2*a);
+        }
+    }
 }
