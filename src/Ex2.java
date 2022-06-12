a291a1f4384fdb0167bff7b099a4b117a74c14de
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-12 16:24:07 +0200

    Fix missing ; in Ex2

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,7 +1,7 @@
 public class Ex2 {
     public static void main(String[] args) {
         float diametre=10;
-        float rayon =diametre/2
+        float rayon =diametre/2;
         double surface = rayon*rayon*Math.PI;
         System.out.println(surface);
     }
