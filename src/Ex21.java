77d518dd41a35ec3ca0b570c7eb4bc03b09f3240
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-13 17:10:46 +0200

    Ex21

---
+++ src/Ex21.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex21 {
+    public static void main(String[] args) {
+        var languages=new ArrayList<String>();
+        var inp = new Scanner(System.in);
+        System.out.println("Entrez des langage de programations");
+        while (true){
+            var language = inp.nextLine();
+            if (language.isBlank()){
+               break;
+            }
+            languages.add(language);
+        }
+        System.out.println(languages);
+        System.out.println("Entrez des langage de programations à effacer");
+        while (true) {
+            var language = inp.nextLine();
+            if (language.isBlank()) {
+                break;
+            }
+            if (languages.contains(language)){
+                languages.remove(language);
+            }
+            else {
+                System.out.printf("%s n'est pas dans la liste des langages de programation défini\n",language);
+            }
+        }
+        System.out.println(languages);
+    }
+}
