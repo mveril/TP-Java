b349d03f2a6e26cbbdd3f7b56423ce250cffa130
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-13 15:07:44 +0200

    Avoid using SringBuilder

--- src/Ex17.java
+++ src/Ex17.java
@@ -5,10 +5,13 @@ public class Ex17 {
         var inp = new Scanner(System.in);
         System.out.printf("Entrer une chaine à inverser :");
         var str=inp.nextLine();
-        var builder = new StringBuilder();
+        //var builder = new StringBuilder();
+        var newStr="";
         for (int i = str.length() -1; i >=0 ; i--) {
-            builder.append(str.charAt(i));
+            //builder.append(str.charAt(i));
+            newStr+=str.charAt(i);
         }
-        System.out.println(builder.toString());
+        //System.out.println(builder.toString());
+        System.out.println(newStr);
     }
 }
--- src/Ex18.java
+++ src/Ex18.java
@@ -5,11 +5,14 @@ public class Ex18 {
         var inp = new Scanner(System.in);
         System.out.printf("Entrer une chaine de caractère à tester :");
         var str=inp.nextLine();
-        var builder = new StringBuilder();
+        //var builder = new StringBuilder();
+        var newStr="";
         for (int i = str.length() -1; i >=0 ; i--) {
-            builder.append(str.charAt(i));
+            //builder.append(str.charAt(i));
+            newStr+=str.charAt(i);
         }
-        if (builder.toString().equalsIgnoreCase(str)){
+        if (newStr.equalsIgnoreCase(str)){
+        //if (builder.toString().equalsIgnoreCase(str)){
             System.out.printf("Le mot %s est un palindrome !\n", str);
         } else {
             System.out.printf("Le mot %s n'est pas un palindrome.\n", str);
