b905e3c1a581ddf2b8549c8c4029b86d073fecf0
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-13 14:22:08 +0200

    Ex18

--- README.md
+++ README.md
@@ -210,3 +210,19 @@ Ecrire un programme Java demande à l'utilisateur de saisir un text et qui ensui
 > Votre chaine : mamy
 > 
 > Résultat : ymam
+
+# Exercice 18.
+Ecrire un programme Java demande à l'utilisateur de saisir un text et qui ensuit, renseigne si celui est un palindrome(mot qui se lit de la même dans les 2 sens).
+
+# Ex :
+
+Entrez un text : non
+
+Résultat : non est un palindrome
+
+
+Entrez un text : oui
+
+Résultat : oui n'est pas un palindrome
+
+[C'est quoi un palindrome?](https://www.larousse.fr/dictionnaires/francais/palindrome/57418)
---
+++ src/Ex18.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex18 {
+    public static void main(String[] args) {
+        var inp = new Scanner(System.in);
+        System.out.printf("Entrer une chaine à inverser :");
+        var str=inp.nextLine();
+        var builder = new StringBuilder();
+        for (int i = str.length() -1; i >=0 ; i--) {
+            builder.append(str.charAt(i));
+        }
+        if (builder.toString().equals(str)){
+            System.out.printf("Le mot %s est un palindrome !\n", str);
+        } else {
+            System.out.printf("Le mot %s n'est pas un palindrome.\n", str);
+        }
+
+    }
+}
