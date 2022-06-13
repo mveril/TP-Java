f7dba5b8ff70c1901d345af9817168641134430f
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-13 16:18:44 +0200

    Ex20

---
+++ src/Ex20.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ex20 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+        var inp = new Scanner(System.in);
+        // Ecriture
+        for (int i = 0; i <= matrix.length-1; i++) {
+            for (int j = 0; j <= matrix[i].length-1; j++) {
+                System.out.printf("Entrer la valeur de M[%d,%d] :",i,j);
+                matrix[i][j]=inp.nextInt();
+            }
+        }
+        for (int i = 0; i <matrix.length; i++) {
+            boolean printSpaceBefore=false;
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (printSpaceBefore) {
+                    System.out.print(" ");
+                }
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        var sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum+=matrix[i][i]+matrix[i][matrix.length-i-1];
+        }
+        System.out.printf("La somme de la diagonale est de : %s", sum);
+    }
+}
