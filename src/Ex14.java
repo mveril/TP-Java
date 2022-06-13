8fff436d4ba27d21db45332235288af1e25aa110
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-13 13:43:53 +0200

    Fix Ex14

--- src/Ex14.java
+++ src/Ex14.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Ex14 {
     public static void main(String[] args) {
         var inp = new Scanner(System.in);
         System.out.println("Inscription");
-        System.out.print("Entrer une adresse email : ");
-        var emailInscr = inp.nextLine();
-        System.out.print("Entrer un mot de passe : ");
-        var pwdInscr = inp.nextLine();
+        var emailRegEx= Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        String emailInscr;
+        do {
+            System.out.print("Entrer une adresse email : ");
+            emailInscr =inp.nextLine();
+        } while (!emailRegEx.matcher(emailInscr).matches());
+        var pwdRegEx= Pattern.compile("^\\d{4}$");
+        String pwdInscr;
+        do {
+            System.out.print("Entrer un mot de passe : ");
+            pwdInscr = inp.nextLine();
+        } while (!pwdRegEx.matcher(pwdInscr).matches());
         System.out.println();
         System.out.println("Connection");
         for (byte i=4;i>=0;i--)
