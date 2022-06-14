95ac120ca071072dc24cd0087f807ec112ed542a
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-14 16:25:09 +0200

    Ex24

---
+++ src/Ex24.java
@@ -0,0 +1,21 @@
+public class Ex24 {
+    static boolean isPrefix(String word, String prefix){
+        for (int i = 0; i < prefix.length(); i++) {
+            if (prefix.charAt(i) != word.charAt(i)){
+                return false;
+            }
+        }
+        return  true;
+    }
+    static void print(String word, String prefix){
+        if (isPrefix(word,prefix)){
+            System.out.printf("%s is a prefix of %s\n",prefix,word);
+        } else {
+            System.out.printf("%s is not a prefix of %s\n",prefix,word);
+        }
+    }
+    public static void main(String[] args) {
+        print("banner", "bang");
+        print("hugging", "hug");
+    }
+}
