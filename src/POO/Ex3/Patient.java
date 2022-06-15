c3cb695a09a4de2c91822d5c637707483e146200
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-15 16:03:00 +0200

    Add POO Ex3

---
+++ src/POO/Ex3/Patient.java
@@ -0,0 +1,60 @@
+package POO.Ex3;
+
+public class Patient {
+    private String firstName;
+    private String lastName;
+    private float height;
+    private float weight;
+
+    public Patient(String firstName, String lastName,float height, float weight){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Patient(String firstName, String lastName){
+        this(firstName,lastName,1.60f,50.0f);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float iMC(){
+        return (float)(weight/Math.pow(height,2));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", firstName, lastName);
+    }
+}
---
+++ src/POO/Ex3/Program.java
@@ -0,0 +1,19 @@
+package POO.Ex3;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        var inp = new Scanner(System.in);
+        System.out.print("Nom :");
+        var lastName = inp.nextLine();
+        System.out.print("Prénom :");
+        var firstName = inp.nextLine();
+        System.out.print("Taille :");
+        var height =  inp.nextFloat();
+        System.out.print("Poid :");
+        var weight =  inp.nextFloat();
+        var patient = new Patient(firstName,lastName,height,weight);
+        System.out.printf("Le patient %s mesure %.2f m.\nIl pèse %.2f Kg.\nSon indice de masse corporelle est de %.2f",patient.toString(),patient.getHeight(),patient.getWeight(), patient.iMC());
+    }
+}
